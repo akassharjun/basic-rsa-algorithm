5aba88fbb6784b393e3e4acb69021110f93537f8
Akassharjun Shanmugarajah <akassharjun@gmail.com>
2020-04-04 13:04:06 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,75 @@
+# To create keys for RSA, one does the following steps:
+# 1. Picks (randomly) two large prime numbers and calls them p and q.
+# 2. Calculates their product and calls it n.
+# 3. Calculates the totient of n ; it is simply ( p −1)(q −1).
+# 4. Picks a random integer that is coprime to ) φ(n and calls this e. A simple way is to
+# just pick a random number ) > max( p,q .
+# 5. Calculates (via the Euclidean division algorithm) the multiplicative inverse of e
+# modulo ) φ(n and call this number d. 
+from math import gcd
+import random
+
+def isPrime(x):
+    return 2 in [x,2**x%x]
+
+def coprime(a, b):
+    return gcd(a, b) == 1
+
+p, q = 13, 17
+
+print("p =", p)
+print("q =", q)
+
+n = p * q
+
+print("n =", n)
+
+phi_n = (p-1)*(q-1)
+
+print("φ(n) =", phi_n)
+
+primes = [i for i in range(1,n) if isPrime(i)]
+
+# print("Available Prime Numbers :", primes)
+
+coprimes = []
+
+for x in primes:
+    if (coprime(x, n)):
+        coprimes.append(x)
+
+e = random.choice(coprimes)
+
+print("e =",e)
+
+# d = (n-r) / q
+# 𝑒𝑑≡1(mod𝜑(𝑛)).
+
+# d = (e % (p - 1) * (q - 1)) ^ -1
+
+# 𝑒𝑥+𝜑(𝑛)𝑦=1
+
+# 𝑑𝑒+𝑘𝜑=𝑟,
+# where 𝑟=gcd(𝑒,𝜑) is the smallest positive integer for which such a solution exists.
+
+def modinv(e, phi):
+    d_old = 0; r_old = phi
+    d_new = 1; r_new = e
+    while r_new > 0:
+        a = r_old // r_new
+        (d_old, d_new) = (d_new, d_old - a * d_new)
+        (r_old, r_new) = (r_new, r_old - a * r_new)
+    return d_old % phi if r_old == 1 else None
+
+d = modinv(e, phi_n)
+
+print("d =",d)
+
+print("Public Key is (", n, ",", e, ")")
+print("Private Key is (", n, ",", d, ")")
+
+# encryption (C = M ^ e (mod n))
+
+# print(2^e % n)
+
+# print(29^d % n)
